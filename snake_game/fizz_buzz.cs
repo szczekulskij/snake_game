a8370573d42bcc730db7769f7d49f1da216a5bb2
Jan <szczekulskij@gmail.com>
2020-12-26 19:29:43 +0000

    add fizz_buzz to git

---
+++ snake_game/fizz_buzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace snake_game
+{
+    class fizz_buzz
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 30; i++)
+            {
+                Console.WriteLine(fizz_buzz_func(i));
+            }
+        }
+
+        static string fizz_buzz_func(int i)
+        {
+            if (i % 15 == 0) { return "FizzBuzz"; }
+            if (i % 5 == 0) { return "Fizz"; }
+            if (i % 3 == 0) { return "Buzz"; }
+            return i.ToString();
+        }
+    }
+
+}
